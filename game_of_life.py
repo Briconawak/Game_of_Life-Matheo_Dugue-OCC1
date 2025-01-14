8ada086c490a7d5147fce34686bbdd9b65c117cd
Briconawak <briconawak@gmail.com>
2025-01-14 15:13:23 +0100

    ajout de fonctionnalitées

--- game_of_life.py
+++ game_of_life.py
@@ -1,11 +1,8 @@
-# on crée une matrice de départ n
-# on ajoute les cases
-# on regarde la matrice de départ pour créer la matrice suivante n+1 en respectant les règles du jeu de la vie
-# etc
+# Description: Jeu de la vie de Conway
 
 import time
-import os
-    
+import random
+
 def add_case(matrice, x, y):
     matrice[x][y] = 1
     return matrice
@@ -16,9 +13,24 @@ def create_matrice(n):
         matrice.append([0]*n)
     return matrice
 
+def fill_random_matrice(matrice):
+    for i in range(len(matrice)):
+        for j in range(len(matrice)):
+            matrice[i][j] = random.randint(0, 1)
+    return matrice
+
 def print_matrice(matrice):
-    for line in matrice:
-        print(line)
+    print("+" + "-"*len(matrice)*2 + "+")
+    for i in range(len(matrice)):
+        print("|", end="")
+        for j in range(len(matrice)):
+            if matrice[i][j] == 1:
+                print("■", end=" ")
+            else:
+                print(" ", end=" ")
+        print("|")
+    print("+" + "-"*len(matrice)*2 + "+")
+        
         
 def count_neighbours(matrice, x, y):
     neighbours = 0
@@ -28,6 +40,20 @@ def count_neighbours(matrice, x, y):
                 if matrice[x+i][y+j] == 1:
                     neighbours += 1
     return neighbours
+
+def count_number_of_ones(matrice):
+    count = 0
+    for i in range(len(matrice)):
+        for j in range(len(matrice)):
+            if matrice[i][j] == 1:
+                count += 1
+    return count
+
+def is_matrice_empty(matrice):
+    if count_number_of_ones(matrice) == 0 :
+        return True
+    else:
+        return False
 
 def next_matrice(matrice):
     new_matrice = create_matrice(len(matrice))
@@ -48,21 +74,52 @@ def next_matrice(matrice):
                     new_matrice[i][j] = 0
     return new_matrice
 
+
+
 def main():
-    n = 5
+    test=False
+    while test == False:
+        try:
+            n = int(input("Quelle taille de matrice souhaitez-vous (taille nxn) ? \n"))
+            if n > 0:
+                test = True
+            else:
+                print("Veuillez entrer un nombre positif")
+        except ValueError:
+            print("Veuillez entrer un nombre positif")
+            test=False
+            
+    test = False
+    while test == False:
+        try:
+            nbCycle = int(input("Combien de génération souhaitez-vous ? \n"))
+            if nbCycle > 0:
+                test = True
+            else:
+                print("Veuillez entrer un nombre positif")
+        except ValueError:
+            print("Veuillez entrer un nombre positif")
+            test=False
+
     matrice = create_matrice(n)
-    matrice = add_case(matrice, 1, 2)
-    matrice = add_case(matrice, 2, 2)
-    matrice = add_case(matrice, 3, 2)
+    matrice = fill_random_matrice(matrice)
     
-    nbCycle = 4
     x=0
+    
     while x < nbCycle: 
+        
         print("\n\ngénération n°", x)
         print_matrice(matrice)
         matrice = next_matrice(matrice)
         time.sleep(1)
         x+=1
+        if is_matrice_empty(matrice):
+            print("\n\ngénération n°", x)
+            print_matrice(matrice)
+            matrice = next_matrice(matrice)
+            print("\nLa matrice est vide, la génération s'arrête")
+            x=nbCycle
+        
         
     
 main()
